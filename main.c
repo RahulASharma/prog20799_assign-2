3f5a06116e0523e0d4f19aaf27d92d5657cc85a2
Devon Fazekas <thomas-d@hotmail.ca>
2018-06-18 03:32:20 -0400

    Cleaned up code for presentation purposes.

--- main.c
+++ main.c
@@ -2,31 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-/*This is studpid. Testing a comment.*/
-
->>>>>>> 4956a547013deef77e153ac2b7de1ff45ada59f3
-const unsigned int MAX_SIZE=100;
+const unsigned int MAX_SIZE = 100;
 typedef unsigned int uint;
 
 // This function will be used to swap "pointers".
-void swap(char**, char**);
+void swap(char **, char **);
 
 // Bubble sort function here.
-void bubbleSort(char**, uint);
+void bubbleSort(char **, uint);
 
 // Read quotes from quotes.txt file file and add them to array. Adjust the size as well!
 // Note: size should reflect the number of quotes in the array/quotes.txt file!
-void read_in(char**, uint*);
+void read_in(char **, uint *);
 
 // Print the quotes using array of pointers.
-void print_out(char**, uint);
+void print_out(char **, uint);
 
 // Save the sorted quotes in the output.txt file
-void write_out(char**, uint);
+void write_out(char **, uint);
 
 // Free memory!
-void free_memory(char**, uint);
+void free_memory(char **, uint);
 
 int main() {
 
@@ -35,62 +31,41 @@ int main() {
     char *arr[MAX_SIZE];
 
     // Number of quotes in the file quotes.txt. Must be adjusted when the file is read!
-    uint size=MAX_SIZE; 
-    
+    uint size = MAX_SIZE;
+
     read_in(arr, &size);
 
 //    printf("--- Input:\n");
 //    print_out(arr, size);
-//    
+//
 //    bubbleSort(arr, size);
-//    
-//    printf("--- Output:\n");    
+//
+//    printf("--- Output:\n");
 //    print_out(arr, size);
 //    write_out(arr, size);
-//    
+//
 //    free_memory(arr, size);
-    
+
     return (0);
 }
 
-void read_in(char** str, uint* size){
-    
+void read_in(char **str, uint *size) {
+
    char name[MAX_SIZE][MAX_SIZE];
-   
-    
+
     FILE *quotes;
     quotes = fopen("C:\\Courses\\PROG20799\\NetBeans\\Assignment2\\quotes.txt", "r");
-    
-    if(quotes == NULL){
+
+    if (quotes == NULL) {
         printf("Unable to open file\n");
     }
-    else{
-        int i =0;
-        
-        while ( fgets(name[i], MAX_SIZE, quotes) != NULL ){
-            
-          
-                
-            
+    else {
+        int i = 0;
+
+        while ( fgets(name[i], MAX_SIZE, quotes) != NULL ) {
             printf("%s", name[i]);
         }
-    
+
         fclose(quotes);
     }
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
-
-
-
